fae4657934bca8bd6fc6b03b6ac9858bd2ac5fdd
JuanMorales1025 <106317963+JuanMorales1025@users.noreply.github.com>
2025-02-21 21:20:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,33 @@
+// Create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); 
+const cors = require('cors');
+const fs = require('fs');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Read comments from file
+function readComments() {
+    const data = fs.readFileSync('comments.json', 'utf8');
+    return JSON.parse(data);
+}
+
+// Write comments to file
+function writeComments(comments) {
+    fs.writeFileSync('comments.json', JSON.stringify(comments, null, 2));
+}
+
+// Get all comments
+app.get('/comments', (req, res) => {
+    const comments = readComments();
+    res.json(comments);
+});
+
+// Add a new comment
+app.post('/comments', (req, res) => {
+    const comments = readComments();
+    const newComment = {
+        id: comments.length + 1,
+        text: req.body.text,
